34a35b6d68d02b91ec33d2ef4ea7f626b3ba0e31
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-07-29 18:41:56 +0200

    feat: Add linting.

--- main.py
+++ main.py
@@ -8,63 +8,69 @@ from instagram.client import InstagramAPI
 
 # utils
 
+
 def getIdFromProfileHtml(response):
-  lookFor = 'profilePage_'
-  profileText = response.text
-  indexOfId = profileText.find(lookFor)
-  textStartedWithId = profileText[indexOfId + len(lookFor):]
-  return textStartedWithId[:textStartedWithId.find('"')]
+    lookFor = 'profilePage_'
+    profileText = response.text
+    indexOfId = profileText.find(lookFor)
+    textStartedWithId = profileText[indexOfId + len(lookFor):]
+    return textStartedWithId[:textStartedWithId.find('"')]
+
 
 def checkResultDirectories():
-  path = os.getcwd()
-  resultPath = f"{path}\\results"
-  profileResultPath = f"{resultPath}\\{profileName}"
-  newScrappingPath = f"{profileResultPath}\\{getTime}"
+    path = os.getcwd()
+    resultPath = f"{path}\\results"
+    profileResultPath = f"{resultPath}\\{profileName}"
+    newScrappingPath = f"{profileResultPath}\\{getTime}"
 
-  if not os.path.exists(resultPath):
-    os.mkdir(resultPath)
+    if not os.path.exists(resultPath):
+        os.mkdir(resultPath)
 
-  if not os.path.exists(profileResultPath):
-    os.mkdir(profileResultPath)
+    if not os.path.exists(profileResultPath):
+        os.mkdir(profileResultPath)
 
-  if not os.path.exists(newScrappingPath):
-    os.mkdir(newScrappingPath)
+    if not os.path.exists(newScrappingPath):
+        os.mkdir(newScrappingPath)
 
-  return newScrappingPath
+    return newScrappingPath
 
-def getGraphqlQueryParams(cursor = ''):
-  graphqlVariables = {
-    'id': id,
-    'first': 20,
-    'after': cursor,
-  }
-  graphqlVariablesJson = json.dumps(graphqlVariables)
-  graphqlQueryHash = 'f2405b236d85e8296cf30347c9f08c2a'
 
-  return {
-    'query_hash': graphqlQueryHash,
-    'variables': graphqlVariablesJson,
-  }
+def getGraphqlQueryParams(cursor=''):
+    graphqlVariables = {
+        'id': id,
+        'first': 20,
+        'after': cursor,
+    }
+    graphqlVariablesJson = json.dumps(graphqlVariables)
+    graphqlQueryHash = 'f2405b236d85e8296cf30347c9f08c2a'
 
-def scrapProfile(endCursor = ''):
-  graphqlUrl = f"{instagramBaseUrl}graphql/query/"
-  graphqlQueryParams = getGraphqlQueryParams(endCursor)
-  picturesQueryResponse = requests.get(graphqlUrl, params=graphqlQueryParams)
-  picturesQueryResponseJson = picturesQueryResponse.json()
-  user = picturesQueryResponse.json()['data']['user']
-  pageInfo = user['edge_owner_to_timeline_media']['page_info']
-  posts = user['edge_owner_to_timeline_media']['edges']
+    return {
+        'query_hash': graphqlQueryHash,
+        'variables': graphqlVariablesJson,
+    }
 
-  newScrappingPath = checkResultDirectories()
 
-  for index, post in enumerate(posts):
-    postNode = post['node']
-    urllib.request.urlretrieve(postNode['display_url'], f"{newScrappingPath}\\{postNode['id']}.jpg")
+def scrapProfile(endCursor=''):
+    graphqlUrl = f"{instagramBaseUrl}graphql/query/"
+    graphqlQueryParams = getGraphqlQueryParams(endCursor)
+    picturesQueryResponse = requests.get(graphqlUrl, params=graphqlQueryParams)
+    picturesQueryResponseJson = picturesQueryResponse.json()
+    user = picturesQueryResponse.json()['data']['user']
+    pageInfo = user['edge_owner_to_timeline_media']['page_info']
+    posts = user['edge_owner_to_timeline_media']['edges']
 
-  pp.pprint(f"# {len(posts)} pictures have been saved.")
-  
-  if pageInfo['has_next_page']:
-    scrapProfile(pageInfo['end_cursor'])
+    newScrappingPath = checkResultDirectories()
+
+    for index, post in enumerate(posts):
+        postNode = post['node']
+        urllib.request.urlretrieve(
+            postNode['display_url'], f"{newScrappingPath}\\{postNode['id']}.jpg")
+
+    pp.pprint(f"# {len(posts)} pictures have been saved.")
+
+    if pageInfo['has_next_page']:
+        scrapProfile(pageInfo['end_cursor'])
+
 
 # init
 pp = pprint.PrettyPrinter(indent=4)
@@ -79,8 +85,8 @@ id = getIdFromProfileHtml(profileHtmlResponse)
 getTime = time.strftime('%Y.%m.%d-%H%M%S')
 
 try:
-  scrapProfile()
+    scrapProfile()
 except:
-  pp.pprint('[ERROR] Scrapping failed.')
+    pp.pprint('[ERROR] Scrapping failed.')
 
 pp.pprint('[SUCCESS] Scrapping finished.')
