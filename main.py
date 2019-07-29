892661d3a1addb751ec2f4cce3001657e664c9c7
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-07-29 04:17:36 +0200

    feat: Add scrapping all picture of user profile.

--- main.py
+++ main.py
@@ -6,6 +6,66 @@ import time
 import urllib.request
 from instagram.client import InstagramAPI
 
+# utils
+
+def getIdFromProfileHtml(response):
+  lookFor = 'profilePage_'
+  profileText = response.text
+  indexOfId = profileText.find(lookFor)
+  textStartedWithId = profileText[indexOfId + len(lookFor):]
+  return textStartedWithId[:textStartedWithId.find('"')]
+
+def checkResultDirectories():
+  path = os.getcwd()
+  resultPath = f"{path}\\results"
+  profileResultPath = f"{resultPath}\\{profileName}"
+  newScrappingPath = f"{profileResultPath}\\{getTime}"
+
+  if not os.path.exists(resultPath):
+    os.mkdir(resultPath)
+
+  if not os.path.exists(profileResultPath):
+    os.mkdir(profileResultPath)
+
+  if not os.path.exists(newScrappingPath):
+    os.mkdir(newScrappingPath)
+
+  return newScrappingPath
+
+def getGraphqlQueryParams(cursor = ''):
+  graphqlVariables = {
+    'id': id,
+    'first': 20,
+    'after': cursor,
+  }
+  graphqlVariablesJson = json.dumps(graphqlVariables)
+  graphqlQueryHash = 'f2405b236d85e8296cf30347c9f08c2a'
+
+  return {
+    'query_hash': graphqlQueryHash,
+    'variables': graphqlVariablesJson,
+  }
+
+def scrapProfile(endCursor = ''):
+  graphqlUrl = f"{instagramBaseUrl}graphql/query/"
+  graphqlQueryParams = getGraphqlQueryParams(endCursor)
+  picturesQueryResponse = requests.get(graphqlUrl, params=graphqlQueryParams)
+  picturesQueryResponseJson = picturesQueryResponse.json()
+  user = picturesQueryResponse.json()['data']['user']
+  pageInfo = user['edge_owner_to_timeline_media']['page_info']
+  posts = user['edge_owner_to_timeline_media']['edges']
+
+  newScrappingPath = checkResultDirectories()
+
+  for index, post in enumerate(posts):
+    postNode = post['node']
+    urllib.request.urlretrieve(postNode['display_url'], f"{newScrappingPath}\\{postNode['id']}.jpg")
+
+  pp.pprint(f"# {len(posts)} pictures have been saved.")
+  
+  if pageInfo['has_next_page']:
+    scrapProfile(pageInfo['end_cursor'])
+
 # init
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -13,52 +73,14 @@ pp = pprint.PrettyPrinter(indent=4)
 instagramBaseUrl = 'https://www.instagram.com/'
 profileName = input('Instagram profile NAME: ')
 profileUrl = f"{instagramBaseUrl}{profileName}/"
+profileHtmlResponse = requests.get(profileUrl)
 
-lookFor = 'profilePage_'
-scrapProfileResponse = requests.get(profileUrl)
-profileText = scrapProfileResponse.text
-indexOfId = profileText.find(lookFor)
-textStartedWithId = profileText[indexOfId + len(lookFor):]
+id = getIdFromProfileHtml(profileHtmlResponse)
+getTime = time.strftime('%Y.%m.%d-%H%M%S')
 
-id = textStartedWithId[:textStartedWithId.find('"')]
+try:
+  scrapProfile()
+except:
+  pp.pprint('[ERROR] Scrapping failed.')
 
-graphqlUrl = f"{instagramBaseUrl}graphql/query/"
-graphqlVariables = {
-  'id': id,
-  'first': 100,
-  'after': '',
-}
-graphqlVariablesJson = json.dumps(graphqlVariables)
-graphqlQueryHash = 'f2405b236d85e8296cf30347c9f08c2a'
-
-graphqlQueryParams = {
-  'query_hash': graphqlQueryHash,
-  'variables': graphqlVariablesJson,
-}
-
-picturesQueryResponse = requests.get(graphqlUrl, params=graphqlQueryParams)
-picturesQueryResponseJson = picturesQueryResponse.json()
-
-user = picturesQueryResponse.json()['data']['user']
-posts = user['edge_owner_to_timeline_media']['edges']
-
-path = os.getcwd()
-resultPath = f"{path}\\results"
-profileResultPath = f"{resultPath}\\{profileName}"
-newScrappingPath = f"{profileResultPath}\\{time.strftime('%Y.%m.%d-%H%M%S')}"
-
-if not os.path.exists(resultPath):
-  os.mkdir(resultPath)
-
-if not os.path.exists(profileResultPath):
-  os.mkdir(profileResultPath)
-
-if not os.path.exists(newScrappingPath):
-  os.mkdir(newScrappingPath)
-
-pp.pprint(f"### {len(posts)}")
-
-for index, post in enumerate(posts):
-  postNode = post['node']
-  pp.pprint(f"{index}. {postNode['display_url']}")
-  urllib.request.urlretrieve(postNode['display_url'], f"{newScrappingPath}\\{postNode['id']}.jpg")
+pp.pprint('[SUCCESS] Scrapping finished.')
