a51527c6d7ef10c094baedaeb4657ee4aa45d059
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-07-29 02:12:40 +0200

    feat: Add getting pictures of provided user.

--- main.py
+++ main.py
@@ -9,13 +9,30 @@ pp = pprint.PrettyPrinter(indent=4)
 # main
 instagramBaseUrl = 'https://www.instagram.com/'
 profileName = input('Instagram profile NAME: ')
-validUrl = f"{instagramBaseUrl}{profileName}/"
+profileUrl = f"{instagramBaseUrl}{profileName}/"
 
 lookFor = 'profilePage_'
-response = requests.get(validUrl)
-text = response.text
-indexOfId = text.find(lookFor)
-textStartedWithId = text[indexOfId + len(lookFor):]
+scrapProfileResponse = requests.get(profileUrl)
+profileText = scrapProfileResponse.text
+indexOfId = profileText.find(lookFor)
+textStartedWithId = profileText[indexOfId + len(lookFor):]
 
 id = textStartedWithId[:textStartedWithId.find('"')]
-pp.pprint(id)
+
+graphqlUrl = f"{instagramBaseUrl}graphql/query/"
+graphqlVariables = {
+  'id': id,
+  'first': 12,
+  'after': '',
+}
+graphqlVariablesJson = json.dumps(graphqlVariables)
+graphqlQueryHash = 'f2405b236d85e8296cf30347c9f08c2a'
+
+graphqlQueryParams = {
+  'query_hash': graphqlQueryHash,
+  'variables': graphqlVariablesJson,
+}
+
+picturesQueryResponse = requests.get(graphqlUrl, params=graphqlQueryParams)
+
+pp.pprint(picturesQueryResponse.json())
